7c45b7df861afb839511838ae90a87df5cf555e5
zalish-shawon <zalish.shawon@gmail.com>
2025-10-09 08:36:03 +0600

    updated

--- dist/controllers/auth.controller.js
+++ dist/controllers/auth.controller.js
@@ -23,7 +23,6 @@ const login = async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "lax",
-        path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.json({
